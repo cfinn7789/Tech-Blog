7172c7fbb3be6fdda682d776957d15163dbc96e3
cfinn7789 <cfinn7789@gmail.com>
2023-08-09 18:43:04 -0400

    added routes

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes');
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
