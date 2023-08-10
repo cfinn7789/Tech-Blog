5e86e933c5e154ace058b08d6b0ce1dbf64aa436
cfinn7789 <cfinn7789@gmail.com>
2023-08-10 17:39:24 -0400

    fixed something i forgot what

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Secretly im a little bit naive',
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
